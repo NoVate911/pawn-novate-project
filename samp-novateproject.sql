7dcc6f0fe3be89d4ca79178755399b0a10aa6076
NoVate <ritcot-delovoy@yandex.ru>
2023-07-10 16:47:01 +0300

    A new database has been uploaded

--- samp-novateproject.sql
+++ samp-novateproject.sql
@@ -22,13 +22,42 @@ USE `samp-novateproject`;
 -- Дамп структуры для таблица samp-novateproject.accounts
 CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nick` varchar(24) DEFAULT NULL,
+  `nick` varchar(24) DEFAULT 'NoVate',
   `password` varchar(32) DEFAULT NULL,
-  `email` varchar(64) DEFAULT NULL,
-  `regIP` varchar(15) DEFAULT NULL,
-  `lastIP` varchar(15) DEFAULT NULL,
-  `regDate` varchar(12) DEFAULT NULL,
-  `lastDate` varchar(12) DEFAULT NULL,
+  `email` varchar(64) DEFAULT 'admin@yandex.ru',
+  `regIP` varchar(15) DEFAULT '0.0.0.0',
+  `lastIP` varchar(15) DEFAULT '0.0.0.0',
+  `regDate` varchar(21) DEFAULT '1999.01.01 / 00:00:00',
+  `lastDate` varchar(21) DEFAULT '1999.01.01 / 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица samp-novateproject.characters
+CREATE TABLE IF NOT EXISTS `characters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(24) DEFAULT 'NoVate',
+  `firstname` varchar(24) DEFAULT 'Jony',
+  `lastname` varchar(24) DEFAULT 'Quatro',
+  `age` int(2) DEFAULT 18,
+  `sex` int(1) DEFAULT 0,
+  `race` int(1) DEFAULT 0,
+  `skin` int(3) DEFAULT 0,
+  `level` int(3) DEFAULT 0,
+  `exp` int(3) DEFAULT 0,
+  `positionX` float DEFAULT 0,
+  `positionY` float DEFAULT 0,
+  `positionZ` float DEFAULT 0,
+  `rotation` float DEFAULT 0,
+  `interior` int(3) DEFAULT 0,
+  `vw` int(10) DEFAULT 0,
+  `health` float DEFAULT 100,
+  `armour` float DEFAULT 0,
+  `regIP` varchar(15) DEFAULT '0.0.0.0',
+  `lastIP` varchar(15) DEFAULT '0.0.0.0',
+  `regDate` varchar(21) DEFAULT '1999.01.01 / 00:00:00',
+  `lastDate` varchar(21) DEFAULT '1999.01.01 / 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
